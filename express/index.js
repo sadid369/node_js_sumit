282e5076af09d222ad710426d55f640f93f74d90
Sadid <sadid.jones@gmail.com>
2022-12-02 20:31:04 +0600

    @02/12/2022

--- express/index.js
+++ express/index.js
@@ -1,21 +1,41 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
-app.set("view engine", "ejs");
+const adminRoute = express.Router();
 
-app
-  .route("/about")
-  .get((req, res) => {
-    res.render("pages/about");
-  })
-  .post((req, res) => {
-    res.send("POST");
-  })
-  .put((req, res) => {
-    res.send("PUT");
-  });
+adminRoute.get("/dashboard", (req, res) => {
+  // console.log(req.originalUrl);
+  // console.log(req.url);
+  // console.log(req.path);
+  // console.log(req.hostname);
+  // console.log(req.ip);
+  console.log(req.params.id);
+  console.log(req.cookies);
+  res.send("We are in admin dashboard");
+});
+app.use("/admin", adminRoute);
+app.get("/user/:id", (req, res) => {
+  // console.log(req.originalUrl);
+  // console.log(req.url);
+  // console.log(req.path);
+  // console.log(req.ip);
+  // console.log(req.hostname);
+  // console.log(req.method);
+  // console.log(req.params);
+  // console.log(req.params.id);
+  // console.log(req.query.filter);
+  console.log(req.cookies);
 
+  res.send("Hello World");
+});
+app.post("/user/", (req, res) => {
+  console.log(req.body);
+  res.send("Hello World Post");
+});
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
 });
