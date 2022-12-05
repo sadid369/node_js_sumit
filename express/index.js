c0586612990cd6bf80a79ba509278e97204559a3
Sadid <sadid.jones@gmail.com>
2022-12-05 21:27:49 +0600

    @5/12/2022

--- express/index.js
+++ express/index.js
@@ -1,10 +1,78 @@
 const express = require("express");
-const adminRouter = require("./adminRouter");
-const publicRouter = require("./publicRouter");
+const multer = require("multer");
+const path = require("path");
+const UPLOADS_FOLDER = "./upload/";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, UPLOADS_FOLDER);
+  },
+  filename: (req, file, cb) => {
+    const fileExt = path.extname(file.originalname);
+    const fileName =
+      file.originalname
+        .replace(fileExt, "")
+        .toLowerCase()
+        .split(" ")
+        .join("-") +
+      "-" +
+      Date.now();
+    cb(null, fileName + fileExt);
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.fieldname === "avatar") {
+      if (
+        file.mimetype === "image/jpeg" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/png"
+      ) {
+        cb(null, true);
+      } else {
+        cb(new Error("Only .jpg, .png or .jpeg format allowed"));
+      }
+    } else if (file.fieldname === "doc") {
+      if (file.mimetype === "application/pdf") {
+        cb(null, true);
+      } else {
+        cb(new Error("Only .pdf type is allowed"));
+      }
+    } else {
+      cb(new Error("There was an unknown error!"));
+    }
+  },
+});
+
 const app = express();
 
-app.use("/admin", adminRouter);
-app.use("/", publicRouter);
+app.post(
+  "/",
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "doc", maxCount: 1 },
+  ]),
+  (req, res) => {
+    console.log(req.files);
+    res.send("Hello world");
+  }
+);
+app.use((err, req, res, next) => {
+  if (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(500).send("There was an upload error!");
+    } else {
+      res.status(500).send(err.message);
+    }
+  } else {
+    res.status(200).send("Success");
+  }
+});
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
 });
