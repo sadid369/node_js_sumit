268693419cf4b64c200d736d28008b27c9981b7e
Sadid <sadid.jones@gmail.com>
2022-12-08 21:24:31 +0600

    @8/12/2022 @31:22 part 26

---
+++ mongodb/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const todoHandler = require("./routeHandaler/todoHandler");
+const app = express();
+app.use(express.json());
+//database connection with mongoose3
+mongoose
+  .connect("mongodb://127.0.0.1:27017/todos")
+  .then(() => console.log("Connection Successful"))
+  .catch((err) => console.log(err));
+
+//application route
+app.use("./todo", todoHandler);
+//default Error
+function errorHandler(err, req, res, next) {
+  if (res.headerSent) {
+    return next(err);
+  }
+  res.status(500).json({ error: err });
+}
+
+app.listen(3000, () => {
+  console.log("app listening at port 3000");
+});
